4435a00652c30fb6b264f4ebb911498836bbcaf9
shanahanjrs <shanahan.jrs@gmail.com>
2017-04-07 22:00:11 -0400

    Added the PID class to control the viki daemon pid lockfile

--- src/_conf.py
+++ src/_conf.py
@@ -9,10 +9,12 @@ Provides Viki configuration information.
 :license: Apache2, see LICENSE for more details. 
 """
 
-__config_home__ = "/usr/local/viki"
-__config_jobs_dir__ = __config_home__ + "/" + "jobs"
+import os
+
+__config_home__ = "{}/.viki".format(os.path.expanduser("~"))
+__config_jobs_dir__ = __config_home__ + "/jobs"
 __config_filename__ = "viki.json"
 __config_file_path__ = __config_home__ + "/" + __config_filename__
-__logfile_path__ = __config_home__ + "/" + "logs"
+__logfile_path__ = __config_home__ + "/logs"
 
 __all__ = ["__config_home__", "__config_jobs_dir__", "__config_filename__", "__config_file_path__", "__logfile_path__" ]
---
+++ src/pid/pid.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-  
+
+""" 
+pid.py
+ ~~~~~
+
+ProcessId library - internal to Viki
+:license: Apache2, see LICENSE for more details. 
+"""
+
+import os
+from src.application.app import App
+
+class Lock(object):
+    """ Lock/pid file library for viki """
+
+
+    def __init__(self):
+        """
+        Initialize lockfile handler
+        """
+
+        application = App()
+
+        self.pidfile_name = "vikid.pid"
+        self.pid_file_abs_path = "{home_dir}/{filename}".format(home_dir=application.home, filename=self.pidfile_name)
+        self.pid = os.getpid()
+
+
+    def pid_file_exists(self):
+        """
+        Check if a current Vikid PID file exists
+        """
+        if os.path.exists(self.pid_file_abs_path):
+            return True
+
+        return False
+
+
+    def pid_file_create(self):
+        """
+        Create a new PID file
+        """
+        with open(self.pid_file_abs_path, mode='w') as pid_file_obj:
+            pid_file_obj.write(self.pid)
+            pid_file_obj.close()
+
+
+    def pid_file_match(self):
+        """
+        Checks the PID file to make sure the written process id
+        matches the current process id.
+        :returns int
+          -1 Mismatch process id
+           0 Missing PID file
+           1 Matching process id
+        """
+        if not self.pid_file_exists():
+            return 0
+
+        with open(self.pid_file_abs_path, 'r') as pid_file_obj:
+            process_id = pid_file_obj.read()
+            pid_file_obj.close()
+
+        if str(self.pid) == process_id:
+            return 1
+        else:
+            return -1
